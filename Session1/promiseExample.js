859a5447fe96bb8f3f082beb5e778ef2e866e92f
ohad.malichi <ohad.malichi@gmail.com>
2019-12-16 19:29:52 +0200

    added new ver

--- Session1/promiseExample.js
+++ Session1/promiseExample.js
@@ -16,6 +16,27 @@ function ASyncSum(arr)
     return prom;
 }
 
-ASyncSum(arr).then(result => console.log(result));
+function useAsyncCodeViaTheThenKeyword()
+{
+    console.log("In function: useAsyncCodeViaTheThenKeyword");
+    ASyncSum(arr).then(sum => console.log(sum));
+    console.log("Ended function: useAsyncCodeViaTheThenKeyword");
+}
 
+
+async function useAsyncCodeViaES8AsyncAwaitFeature()
+{
+    console.log("In function: useAsyncCodeViaES8AsyncAwaitFeature");
+    
+    var sum = await ASyncSum(arr);
+
+    console.log(sum);
+
+    console.log("Ended function: useAsyncCodeViaES8AsyncAwaitFeature");
+}
+
+
+console.log("Start of code");
+useAsyncCodeViaTheThenKeyword();
+useAsyncCodeViaES8AsyncAwaitFeature()
 console.log("End of code");
